47936573a76b825a59b175eb18a01c90433ab0e2
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2014-05-25 16:00:13 +0500

    converted select html options to ng-options

--- client/scripts/accounts/transactionsCtrl.js
+++ client/scripts/accounts/transactionsCtrl.js
@@ -3,7 +3,8 @@
 angular.module('NigshahOM').controller('transactionsCtrl', ['$scope','$stateParams','transactionsServ', function ($scope, $stateParams, transactionsServ) {
 	var accountNumber = $stateParams.accountNumber;
 	$scope.allTransactions = transactionsServ.GetAllTransactionsForAccount(accountNumber);
-
+	$scope.transactionTypes = transactionsServ.GetTransactionTypes();
+	$scope.transactionModes = transactionsServ.GetTransactionModes();
 	$scope.addNewTransaction = function (transaction) {
 	    transactionsServ.PostTransactionForAccount(accountNumber, transaction).then(function (newTransaction) {
 	        $scope.allTransactions.Transactions.push(newTransaction);
--- client/scripts/accounts/transactionsServ.js
+++ client/scripts/accounts/transactionsServ.js
@@ -4,6 +4,31 @@ angular.module('NigshahOM').factory('transactionsServ', ['$resource', function (
 	
 	var methods = { getAll: {method: 'GET'}, put: {method: 'PUT'}};
 	var TransactionsResource = $resource('/api/accounts/:id/transactions', { id: '@id' }, methods);
+	var transactionTypes = [
+        {
+            name:   'Credit',
+            value:  'credit'
+        },
+        {
+            name: 'Debit',
+            value: 'debit'
+        }
+	]
+
+	var transactionModes = [
+        {
+            name:  'Cash',
+            value: 'cash'
+        },
+        {
+            name: 'Cheque',
+            value: 'cheque'
+        },
+        {
+            name: 'Online',
+            value: 'online'
+        }
+	]
 
 	return {
 		GetAllTransactionsForAccount: function(accountId){
@@ -11,6 +36,12 @@ angular.module('NigshahOM').factory('transactionsServ', ['$resource', function (
 		},
 		PostTransactionForAccount: function (accountId, addedtransaction) {
 		    return TransactionsResource.save({ id: accountId }, { transaction: addedtransaction }).$promise;
+		},
+		GetTransactionTypes: function () {
+		    return transactionTypes;
+		},
+		GetTransactionModes: function () {
+		    return transactionModes;
 		}
 	};
 }])
